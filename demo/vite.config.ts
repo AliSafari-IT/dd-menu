8e68d8793bfa636199d7e73f6f52a87180de5795
Ali Safari <asafarim.it@gmail.com>
2025-08-09 17:22:18 +0200

    chore: add Vite config files for demo app with React plugin and port settings

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/dd-menu/",
   plugins: [react()],
   server: {
     port: 3009
